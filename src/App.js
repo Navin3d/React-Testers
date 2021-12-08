247db704ba3234d3e0d9e6b164f339028c71d4d8
S M Navin Durai <71096790+Navin3d@users.noreply.github.com>
2021-12-08 09:43:19 +0530

    This is the testing application of backend-connectivity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import DotLoader from "react-spinners/DotLoader";
 import './App.css';
 
 function App() {
+  const [plans, setPlan] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("http://3.17.135.71:8080/api/plan/get_all_plans/");
+      await setPlan(response.data);
+      console.log("Response: ", JSON.stringify(response.data));
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <DotLoader color="red" loading={loading} size={50} />}
+      {plans.map(plan => (<div key={plan.id}>{`${plan.description}`}</div>))}
+
+      {error !== "" && `${error}`}
     </div>
   );
 }
