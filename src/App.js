2f558213638ca6f479d9d362cc54ec22aa3a06a7
Navin3d <smnavin65@gmail.com>
2021-12-15 22:56:05 +0530

    only console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import DotLoader from "react-spinners/DotLoader";
+import ClockLoader from "react-spinners/ClockLoader";
 import './App.css';
 
 function App() {
@@ -16,7 +16,18 @@ function App() {
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await axios.get("http://3.17.135.71:8080/api/plan/get_all_plans/");
+      // const response = await axios.get("http://3.17.135.71:8080/api/plan/get_all_plans/");
+
+      const headers = {
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5NDQyODA3MjE3IiwiZXhwIjoxNjQwMjc2MTc5LCJpYXQiOjE2Mzk0MTIxNzl9.Y0xjcw3MzbpUDisTPmwmOKW5EjP7XGcLM7cQJuC7sQI"
+      }
+
+      // const response = await axios.get("http://localhost:8000/fitness/getexerciselistfor7days?date=2021-09-11&user_id=9442807217", { headers });
+
+      // const response = await axios.get("http://3.17.135.71:8080/getuser", { headers });
+
+      const response = await axios.get("http://3.17.135.71:8000/api/questions/get_questions?sports=1002", { headers });
+
       await setPlan(response.data);
       console.log("Response: ", JSON.stringify(response.data));
     } catch (err) {
@@ -28,8 +39,8 @@ function App() {
 
   return (
     <div className="App">
-      {loading && <DotLoader color="red" loading={loading} size={50} />}
-      {plans.map(plan => (<div key={plan.id}>{`${plan.description}`}</div>))}
+      <ClockLoader color="red" loading={loading} size={500} />
+      {/* {plans.map(plan => (<div key={plan.id}>{`${plan.description}`}</div>))} */}
 
       {error !== "" && `${error}`}
     </div>
